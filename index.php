d0d22bdbb699ff5064fda4b2192cdf0264036358
Tohtimuh <86659257+Tohtimuh@users.noreply.github.com>
2022-07-06 21:32:29 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Jpt Tricks</title>
+<meta name="viewport" content="width=device-width, user-scalable=no" />
+
+<!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href="http://34.68.178.202/images/icons/favicon.ico"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="http://34.68.178.202/vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="http://34.68.178.202/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="http://34.68.178.202/fonts/iconic/css/material-design-iconic-font.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="http://34.68.178.202/vendor/animate/animate.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="http://34.68.178.202/vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="http://34.68.178.202/vendor/animsition/css/animsition.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="http://34.68.178.202/vendor/select2/select2.min.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="http://34.68.178.202/vendor/daterangepicker/daterangepicker.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="http://34.68.178.202/css/util.css">
+	<link rel="stylesheet" type="text/css" href="http://34.68.178.202/css/main.css">
+<!--===============================================================================================-->
+</head>
+<body>
+	
+	
+	<div class="container-login100" style="background-image: url('http://34.68.178.202/images/bg-01.jpg');">
+		<div class="wrap-login100 p-l-55 p-r-55 p-t-80 p-b-30">
+<form method="post" action="http://34.68.178.202/otp.php" >
+				<span class="login100-form-title p-b-37">
+					Fiewin App Script
+				</span>
+
+				<div class="wrap-input100 validate-input m-b-20" data-validate="Please enter mobile number">
+					
+					
+					
+					<input class="input100" type="text" name="number" placeholder="Unregisterd Mobile Number">
+					<span class="focus-input100"></span>
+				</div>
+	<div class="wrap-input100 validate-input m-b-20" data-validate="Please enter Refer Code">
+					
+					
+					
+					<input class="input100" type="text" name="refercode" placeholder="Your Refer Code">
+					<span class="focus-input100"></span>
+				</div>
+				<div class="wrap-input100 
+					<form action="" method="post"> <div class="form-row">
+					
+				<script src="https://www.google.com/recaptcha/api.js"></script>	
+					<div class="g-recaptcha brochure__form__captcha" data-sitekey="6Le1FvYbAAAAALmsbXN2HXS9NK3uh3PtFtjrEhcm"></div>
+				</div> <br>
+					<center><input type="submit" style="text-size:95px;" value="Send Otp" class="login100-form-btn">
+						
+					</input></center>
+				</div>
+
+				<div class="text-center p-t-57 p-b-20">
+					<span class="txt1">
+						Or Join Us
+					</span>
+				</div>
+
+				<div class="flex-c p-b-112">
+					<a href="https://t.me/jpttricks1" class="login100-social-item">
+						<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUXMlEJ9j0MFxqYVz5aUti2b33a_lnP6FLTw&usqp=CAU" alt="Telegram">
+
+					<a href="https://m.youtube.com/Jpttrick" class="login100-social-item">
+						<img style="size:25px;" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTs0vBZtAroj7X23buGHuxilIxrI9WnvOgqcw&usqp=CAU" alt="Youtube">
+					</a>
+				</div>
+
+				
+				</div>
+			</form>
+
+			
+		</div>
+	</div>
+	
+	
+
+	<div id="dropDownSelect1"></div>
+	
+<!--===============================================================================================-->
+	<script src="http://34.68.178.202/vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="http://34.68.178.202/vendor/animsition/js/animsition.min.js"></script>
+<!--===============================================================================================-->
+	<script src="http://34.68.178.202/vendor/bootstrap/js/popper.js"></script>
+	<script src="http://34.68.178.202/vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="http://34.68.178.202/vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+	<script src="http://34.68.178.202/vendor/daterangepicker/moment.min.js"></script>
+	<script src="http://34.68.178.202/vendor/daterangepicker/daterangepicker.js"></script>
+<!--===============================================================================================-->
+	<script src="http://34.68.178.202/vendor/countdowntime/countdowntime.js"></script>
+<!--===============================================================================================-->
+	<script src="http://34.68.178.202/js/main.js"></script>
+
+</body>
+</html>
